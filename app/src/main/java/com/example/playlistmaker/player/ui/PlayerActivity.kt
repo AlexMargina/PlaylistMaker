7942be3d7ef2f0ed391179321138b89d679e07e0
Git_maalmi <marginali@ya.ru>
2023-10-28 11:05:37 +0300

    7. Corrected center of btPlay

--- app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
@@ -8,7 +8,6 @@ import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityPlayerBinding
 import com.example.playlistmaker.player.domain.PlayerState
 import com.example.playlistmaker.search.domain.TrackModel
-import com.google.android.material.button.MaterialButton
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -17,7 +16,6 @@ import java.util.Locale
 class PlayerActivity : AppCompatActivity() {
 
     private val viewModel by viewModel<PlayerViewModel>()
-    private lateinit var buttonPlay: MaterialButton
     private lateinit var binding: ActivityPlayerBinding
 
 
@@ -26,27 +24,24 @@ class PlayerActivity : AppCompatActivity() {
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel.observatorScreen().observe(this) {
+            refreshScreen(it)
+        }
 
-            buttonPlay = binding.btPlay
+        viewModel.observatorTimer().observe(this) {
+            refreshTime(it)
+        }
 
-            viewModel.observatorScreen().observe(this) {
-                refreshScreen(it)
+
+        assign(getTrack())
+
+        binding.btPlay.setOnClickListener {
+            if (viewModel.isClickAllowed()) {
+                viewModel.playbackControl()
             }
+        }
 
-            viewModel.observatorTimer().observe(this) {
-                refreshTime(it)
-            }
-
-
-            assign(getTrack())
-
-            buttonPlay.setOnClickListener {
-                if (viewModel.isClickAllowed()) {
-                    viewModel.playbackControl()
-                }
-            }
-
-            binding.ivBack.setOnClickListener { finish() }
+        binding.ivBack.setOnClickListener { finish() }
     }
 
     fun refreshTime(time: String) {
@@ -58,40 +53,40 @@ class PlayerActivity : AppCompatActivity() {
         if (viewModel.isNightTheme()) {
             when (state) {
                 is PlayerState.PLAYING -> {
-                    buttonPlay.setIconResource(R.drawable.button_pause_night)
+                    binding.btPlay.setIconResource(R.drawable.button_pause_night)
                 }
 
                 is PlayerState.PAUSED -> {
-                    buttonPlay.setIconResource(R.drawable.button_play_night)
+                    binding.btPlay.setIconResource(R.drawable.button_play_night)
                 }
 
                 is PlayerState.PREPARED -> {
-                    buttonPlay.setIconResource(R.drawable.button_play_night)
-                    buttonPlay.isEnabled = true
+                    binding.btPlay.setIconResource(R.drawable.button_play_night)
+                    binding.btPlay.isEnabled = true
                 }
 
                 else -> {
-                    buttonPlay.setIconResource(R.drawable.button_play_night)
+                    binding.btPlay.setIconResource(R.drawable.button_play_night)
 
                 }
             }
         } else {
             when (state) {
                 is PlayerState.PLAYING -> {
-                    buttonPlay.setIconResource(R.drawable.button_pause_day)
+                    binding.btPlay.setIconResource(R.drawable.button_pause_day)
                 }
 
                 is PlayerState.PAUSED -> {
-                    buttonPlay.setIconResource(R.drawable.button_play_day)
+                    binding.btPlay.setIconResource(R.drawable.button_play_day)
                 }
 
                 is PlayerState.PREPARED -> {
-                    buttonPlay.setIconResource(R.drawable.button_play_day)
-                    buttonPlay.isEnabled = true
+                    binding.btPlay.setIconResource(R.drawable.button_play_day)
+                    binding.btPlay.isEnabled = true
                 }
 
                 else -> {
-                    buttonPlay.setIconResource(R.drawable.button_play_day)
+                    binding.btPlay.setIconResource(R.drawable.button_play_day)
 
                 }
             }
@@ -127,7 +122,7 @@ class PlayerActivity : AppCompatActivity() {
             .transform(RoundedCorners(radius))
             .placeholder(R.drawable.media_placeholder)
             .into(binding.ivCover512)
-        buttonPlay.isEnabled = false
+        binding.btPlay.isEnabled = false
     }
 
 }
